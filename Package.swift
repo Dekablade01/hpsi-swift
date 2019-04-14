d741aa9c35ec92175ad374da5b8364756facf67e
Issarapong Poesua <dekablade01@gmail.com>
2019-04-14 19:05:06 +0700

    install alamofire

--- Package.swift
+++ Package.swift
@@ -9,12 +9,13 @@ let package = Package(
     dependencies: [
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
-
+        // 🔥 Alamofire is an HTTP networking library written in Swift.
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "4.8.2"),
         // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
         .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0")
     ],
     targets: [
-        .target(name: "App", dependencies: ["FluentSQLite", "Vapor"]),
+        .target(name: "App", dependencies: ["FluentSQLite", "Vapor", "Alamofire"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
