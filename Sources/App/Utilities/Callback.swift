a286d475b861a3c1997d521653eea97d7a9e4e93
Issarapong Poesua <dekablade01@gmail.com>
2019-04-14 20:46:44 +0700

    create type alias for completion and callback

---
+++ Sources/App/Utilities/Callback.swift
@@ -0,0 +1,9 @@
+//
+//  Callback.swift
+//  App
+//
+//  Created by Issarapong Poesua on 14/4/19.
+//  Copyright © 2019 SwiftLovers.com. All rights reserved.
+//
+
+typealias Callback<T> = (T) -> Void
