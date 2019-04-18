2797a84b3bade72d1a7af758773c13b79123c0e9
Issarapong Poesua <dekablade01@gmail.com>
2019-04-18 22:05:34 +0700

    request manager support promise

--- Sources/App/Managers/Request Manager/APIRequestManager.swift
+++ Sources/App/Managers/Request Manager/APIRequestManager.swift
@@ -35,13 +35,40 @@ final class APIRequestManager: RequestManager {
         perform(requestable, responseHandler: ResponseHandler<[T]>(), callback: callback)
     }
     
+    func perform<T: Decodable>(_ requestable: Request<T>) -> Promise<T> {
+        let promise = Promise<T>()
+        
+        perform(requestable) {
+            switch $0 {
+            case .success(let value): promise.success(with: value)
+            case .failure(let error): promise.failure(with: error)
+            }
+        }
+        return promise
+    }
+
+    func perform<T: Decodable>(_ requestable: CollectionRequest<T>) -> Promise<[T]> {
+        let promise = Promise<[T]>()
+        perform(requestable) {
+            switch $0 {
+            case .success(let value): promise.success(with: value)
+            case .failure(let error): promise.failure(with: error)
+            }
+        }
+        return promise
+    }
+    
     private func perform<Handler: ResponseHandlable>(
         _ request: Requestable,
         responseHandler: Handler,
         callback: Callback<Swift.Result<Handler.ResponseType, Error>>?) {
         sessionManager
             .request(request.path, method: request.method, parameters: request.parameters, encoding: request.encoding, headers: request.headers)
-            .responseData { responseHandler.handleResponse($0) { callback?($0) } }
+            .responseData { data in
+                responseHandler.handleResponse(data) { result in
+                    callback?(result)
+                }
+        }
     }
 }
 
